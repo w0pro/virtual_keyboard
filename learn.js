2c8331ed6310a8d6ccb83a0425a861b62756dd0c
yansapronov <ya.sapronov@gmail.com>
2022-05-10 02:14:41 +0300

    feat: js function to project

---
+++ learn.js
@@ -0,0 +1,942 @@
+const en = [
+    {
+        code: 'Backquote',
+        width: 60,
+        color: 'yellow',
+        key: '`',
+    },
+    {
+        width: 60,
+        code: 'Digit1',
+        color: 'grey',
+        key: '1',
+    },
+    {
+        width: 60,
+        code: 'Digit2',
+        color: 'grey',
+        key: '2',
+    },
+    {
+        width: 60,
+        code: 'Digit3',
+        color: 'grey',
+        key: '3',
+    },
+    {
+        width: 60,
+        code: 'Digit4',
+        color: 'grey',
+        key: '4',
+    },
+    {
+        width: 60,
+        code: 'Digit5',
+        color: 'grey',
+        key: '5',
+    },
+    {
+        width: 60,
+        code: 'Digit6',
+        color: 'grey',
+        key: '6',
+    },
+    {
+        width: 60,
+        code: 'Digit7',
+        color: 'grey',
+        key: '7',
+    },
+    {
+        width: 60,
+        code: 'Digit8',
+        color: 'grey',
+        key: '8',
+    },
+    {
+        width: 60,
+        code: 'Digit9',
+        color: 'grey',
+        key: '9',
+    },
+    {
+        width: 60,
+        code: 'Digit0',
+        color: 'grey',
+        key: '0',
+    },
+    {
+        code: 'Minus',
+        width: 60,
+        key: '-',
+        color: 'grey',
+    },
+    {
+        code: 'Equal',
+        width: 60,
+        key: '=',
+        color: 'grey',
+    },
+    {
+        code: 'Backspace',
+        width: 100,
+        key: 'Backspace',
+        color: 'yellow',
+    },
+    {
+        code: 'Tab',
+        width: 90,
+        key: 'Tab',
+        color: 'yellow',
+    },
+    {
+        code: 'KeyQ',
+        width: 60,
+        key: 'Q',
+        color: 'grey',
+    },
+    {
+        code: 'KeyW',
+        width: 60,
+        key: 'W',
+        color: 'grey',
+    },
+    {
+        code: 'KeyE',
+        width: 60,
+        key: 'E',
+        color: 'grey',
+    },
+    {
+        code: 'KeyR',
+        width: 60,
+        key: 'R',
+        color: 'grey',
+    },
+    {
+        code: 'KeyT',
+        width: 60,
+        key: 'T',
+        color: 'grey',
+    },
+    {
+        code: 'KeyY',
+        width: 60,
+        key: 'Y',
+        color: 'grey',
+    },
+    {
+        code: 'KeyU',
+        width: 60,
+        key: 'U',
+        color: 'grey',
+    },
+    {
+        code: 'KeyI',
+        width: 60,
+        key: 'I',
+        color: 'grey',
+    },
+    {
+        code: 'KeyO',
+        width: 60,
+        key: 'O',
+        color: 'grey',
+    },
+    {
+        code: 'KeyP',
+        width: 60,
+        key: 'P',
+        color: 'grey',
+    },
+    {
+        code: 'BracketLeft',
+        width: 60,
+        key: '[',
+        color: 'grey',
+    },
+    {
+        code: 'BracketRight',
+        width: 60,
+        key: ']',
+        color: 'grey',
+    },
+    {
+        code: 'NumpadDecimal',
+        width: 70,
+        key: 'DEL',
+        color: 'yellow',
+    },
+    {
+        code: 'CapsLock',
+        width: 100,
+        key: 'Caps Lock',
+        color: 'yellow',
+    },
+    {
+        code: 'KeyA',
+        width: 60,
+        key: 'A',
+        color: 'grey',
+    },
+    {
+        code: 'KeyS',
+        width: 60,
+        key: 'S',
+        color: 'grey',
+    },
+    {
+        code: 'KeyD',
+        width: 60,
+        key: 'D',
+        color: 'grey',
+    },
+    {
+        code: 'KeyF',
+        width: 60,
+        key: 'F',
+        color: 'grey',
+    },
+    {
+        code: 'KeyG',
+        width: 60,
+        key: 'G',
+        color: 'grey',
+    },
+    {
+        code: 'KeyH',
+        width: 60,
+        key: 'H',
+        color: 'grey',
+    },
+    {
+        code: 'KeyJ',
+        width: 60,
+        key: 'J',
+        color: 'grey',
+    },
+    {
+        code: 'KeyK',
+        width: 60,
+        key: 'K',
+        color: 'grey',
+    },
+    {
+        code: 'KeyL',
+        width: 60,
+        key: 'L',
+        color: 'grey',
+    },
+    {
+        code: 'Semicolon',
+        width: 60,
+        key: ';',
+        color: 'grey',
+    },
+    {
+        code: 'Quote',
+        width: 60,
+        key: '\'',
+        color: 'grey',
+    },
+    {
+        code: 'Enter',
+        width: 135,
+        key: 'ENTER',
+        color: 'yellow',
+    },
+    {
+        code: 'ShiftLeft',
+        width: 100,
+        key: 'Shift',
+        color: 'yellow',
+    },
+    {
+        code: 'Backslash',
+        width: 60,
+        key: '\\',
+        color: 'grey',
+    },
+    {
+        code: 'KeyZ',
+        width: 60,
+        key: 'Z',
+        color: 'grey',
+    },
+    {
+        code: 'KeyX',
+        width: 60,
+        key: 'X',
+        color: 'grey',
+    },
+    {
+        code: 'KeyC',
+        width: 60,
+        key: 'C',
+        color: 'grey',
+    },
+    {
+        code: 'KeyV',
+        width: 60,
+        key: 'V',
+        color: 'grey',
+    },
+    {
+        code: 'KeyB',
+        width: 60,
+        key: 'B',
+        color: 'grey',
+    },
+    {
+        code: 'KeyN',
+        width: 60,
+        key: 'N',
+        color: 'grey',
+    },
+    {
+        code: 'KeyM',
+        width: 60,
+        key: 'M',
+        color: 'grey',
+    },
+    {
+        code: 'Period',
+        width: 60,
+        key: '.',
+        color: 'grey',
+    },
+    {
+        code: 'Comma',
+        width: 60,
+        key: ',',
+        color: 'grey',
+    },
+    {
+        code: 'Slash',
+        width: 60,
+        key: '/',
+        color: 'grey',
+    },
+    {
+        code: 'ArrowUp',
+        width: 60,
+        key: '↑',
+        color: 'yellow',
+    },
+    {
+        code: 'ShiftRight',
+        width: 60,
+        key: 'Shift',
+        color: 'yellow',
+    },
+    {
+        code: 'ControlLeft',
+        width: 80,
+        key: 'Ctrl',
+        color: 'yellow',
+    },
+    {
+        code: 'MetaLeft',
+        width: 60,
+        key: '⌘',
+        color: 'yellow',
+    },
+    {
+        code: 'AltLeft',
+        width: 60,
+        key: 'Alt',
+        color: 'yellow',
+    },
+    {
+        code: 'Space',
+        width: 440,
+        key: '',
+        color: 'grey',
+    },
+    {
+        code: 'AltRight',
+        width: 60,
+        key: 'Alt',
+        color: 'yellow',
+    },
+    {
+        code: 'ControlRight',
+        width: 80,
+        key: 'Ctrl',
+        color: 'yellow',
+    },
+    {
+        code: 'ArrowLeft',
+        width: 60,
+        key: '←',
+        color: 'yellow',
+    },
+    {
+        code: 'ArrowDown',
+        width: 60,
+        key: '↓',
+        color: 'yellow',
+    },
+    {
+        code: 'ArrowRight',
+        width: 60,
+        key: '→',
+        color: 'yellow',
+    },
+];
+
+const ru = [
+    {
+        code: 'Backquote',
+        width: 60,
+        color: 'yellow',
+        key: '`',
+    },
+    {
+        width: 60,
+        code: 'Digit1',
+        color: 'grey',
+        key: '1',
+    },
+    {
+        width: 60,
+        code: 'Digit2',
+        color: 'grey',
+        key: '2',
+    },
+    {
+        width: 60,
+        code: 'Digit3',
+        color: 'grey',
+        key: '3',
+    },
+    {
+        width: 60,
+        code: 'Digit4',
+        color: 'grey',
+        key: '4',
+    },
+    {
+        width: 60,
+        code: 'Digit5',
+        color: 'grey',
+        key: '5',
+    },
+    {
+        width: 60,
+        code: 'Digit6',
+        color: 'grey',
+        key: '6',
+    },
+    {
+        width: 60,
+        code: 'Digit7',
+        color: 'grey',
+        key: '7',
+    },
+    {
+        width: 60,
+        code: 'Digit8',
+        color: 'grey',
+        key: '8',
+    },
+    {
+        width: 60,
+        code: 'Digit9',
+        color: 'grey',
+        key: '9',
+    },
+    {
+        width: 60,
+        code: 'Digit0',
+        color: 'grey',
+        key: '0',
+    },
+    {
+        code: 'Minus',
+        width: 60,
+        key: '-',
+        color: 'grey',
+    },
+    {
+        code: 'Equal',
+        width: 60,
+        key: '=',
+        color: 'grey',
+    },
+    {
+        code: 'Backspace',
+        width: 100,
+        key: 'Backspace',
+        color: 'yellow',
+    },
+    {
+        code: 'Tab',
+        width: 90,
+        key: 'Tab',
+        color: 'yellow',
+    },
+    {
+        code: 'KeyQ',
+        width: 60,
+        key: 'Й',
+        color: 'grey',
+    },
+    {
+        code: 'KeyW',
+        width: 60,
+        key: 'Ц',
+        color: 'grey',
+    },
+    {
+        code: 'KeyE',
+        width: 60,
+        key: 'У',
+        color: 'grey',
+    },
+    {
+        code: 'KeyR',
+        width: 60,
+        key: 'К',
+        color: 'grey',
+    },
+    {
+        code: 'KeyT',
+        width: 60,
+        key: 'Е',
+        color: 'grey',
+    },
+    {
+        code: 'KeyY',
+        width: 60,
+        key: 'Н',
+        color: 'grey',
+    },
+    {
+        code: 'KeyU',
+        width: 60,
+        key: 'Г',
+        color: 'grey',
+    },
+    {
+        code: 'KeyI',
+        width: 60,
+        key: 'Ш',
+        color: 'grey',
+    },
+    {
+        code: 'KeyO',
+        width: 60,
+        key: 'Щ',
+        color: 'grey',
+    },
+    {
+        code: 'KeyP',
+        width: 60,
+        key: 'З',
+        color: 'grey',
+    },
+    {
+        code: 'BracketLeft',
+        width: 60,
+        key: 'Х',
+        color: 'grey',
+    },
+    {
+        code: 'BracketRight',
+        width: 60,
+        key: 'Ъ',
+        color: 'grey',
+    },
+    {
+        code: 'NumpadDecimal',
+        width: 70,
+        key: 'DEL',
+        color: 'yellow',
+    },
+    {
+        code: 'CapsLock',
+        width: 100,
+        key: 'Caps Lock',
+        color: 'yellow',
+    },
+    {
+        code: 'KeyA',
+        width: 60,
+        key: 'Ф',
+        color: 'grey',
+    },
+    {
+        code: 'KeyS',
+        width: 60,
+        key: 'Ы',
+        color: 'grey',
+    },
+    {
+        code: 'KeyD',
+        width: 60,
+        key: 'В',
+        color: 'grey',
+    },
+    {
+        code: 'KeyF',
+        width: 60,
+        key: 'А',
+        color: 'grey',
+    },
+    {
+        code: 'KeyG',
+        width: 60,
+        key: 'П',
+        color: 'grey',
+    },
+    {
+        code: 'KeyH',
+        width: 60,
+        key: 'Р',
+        color: 'grey',
+    },
+    {
+        code: 'KeyJ',
+        width: 60,
+        key: 'О',
+        color: 'grey',
+    },
+    {
+        code: 'KeyK',
+        width: 60,
+        key: 'Л',
+        color: 'grey',
+    },
+    {
+        code: 'KeyL',
+        width: 60,
+        key: 'Д',
+        color: 'grey',
+    },
+    {
+        code: 'Semicolon',
+        width: 60,
+        key: 'Ж',
+        color: 'grey',
+    },
+    {
+        code: 'Quote',
+        width: 60,
+        key: 'Ё',
+        color: 'grey',
+    },
+    {
+        code: 'Enter',
+        width: 135,
+        key: 'ENTER',
+        color: 'yellow',
+    },
+    {
+        code: 'ShiftLeft',
+        width: 100,
+        key: 'Shift',
+        color: 'yellow',
+    },
+    {
+        code: 'Backslash',
+        width: 60,
+        key: 'Э',
+        color: 'grey',
+    },
+    {
+        code: 'KeyZ',
+        width: 60,
+        key: 'Я',
+        color: 'grey',
+    },
+    {
+        code: 'KeyX',
+        width: 60,
+        key: 'Ч',
+        color: 'grey',
+    },
+    {
+        code: 'KeyC',
+        width: 60,
+        key: 'С',
+        color: 'grey',
+    },
+    {
+        code: 'KeyV',
+        width: 60,
+        key: 'М',
+        color: 'grey',
+    },
+    {
+        code: 'KeyB',
+        width: 60,
+        key: 'И',
+        color: 'grey',
+    },
+    {
+        code: 'KeyN',
+        width: 60,
+        key: 'Т',
+        color: 'grey',
+    },
+    {
+        code: 'KeyM',
+        width: 60,
+        key: 'Ь',
+        color: 'grey',
+    },
+    {
+        code: 'Period',
+        width: 60,
+        key: 'Б',
+        color: 'grey',
+    },
+    {
+        code: 'Comma',
+        width: 60,
+        key: 'Ю',
+        color: 'grey',
+    },
+    {
+        code: 'Slash',
+        width: 60,
+        key: '/',
+        color: 'grey',
+    },
+    {
+        code: 'ArrowUp',
+        width: 60,
+        key: '↑',
+        color: 'yellow',
+    },
+    {
+        code: 'ShiftRight',
+        width: 60,
+        key: 'Shift',
+        color: 'yellow',
+    },
+    {
+        code: 'ControlLeft',
+        width: 80,
+        key: 'Ctrl',
+        color: 'yellow',
+    },
+    {
+        code: 'MetaLeft',
+        width: 60,
+        key: '⌘',
+        color: 'yellow',
+    },
+    {
+        code: 'AltLeft',
+        width: 60,
+        key: 'Alt',
+        color: 'yellow',
+    },
+    {
+        code: 'Space',
+        width: 440,
+        key: '',
+        color: 'grey',
+    },
+    {
+        code: 'AltRight',
+        width: 60,
+        key: 'Alt',
+        color: 'yellow',
+    },
+    {
+        code: 'ControlRight',
+        width: 80,
+        key: 'Ctrl',
+        color: 'yellow',
+    },
+    {
+        code: 'ArrowLeft',
+        width: 60,
+        key: '←',
+        color: 'yellow',
+    },
+    {
+        code: 'ArrowDown',
+        width: 60,
+        key: '↓',
+        color: 'yellow',
+    },
+    {
+        code: 'ArrowRight',
+        width: 60,
+        key: '→',
+        color: 'yellow',
+    },
+];
+
+const body = document.querySelector('body');
+// Создаем блок с панелью
+const panel = document.createElement('div');
+panel.classList.add('panel');
+body.append(panel);
+// Кнопка открытия панели
+const openKeyboard = document.createElement('button');
+openKeyboard.classList.add('open');
+openKeyboard.innerText = 'Открыть клавиатуру';
+body.append(openKeyboard);
+
+// Флаг состояния клавиатуры
+let isBtn = false;
+
+// Создаем текстовое воле
+const textarea = document.createElement('textarea');
+textarea.style.width = '700px';
+textarea.style.height = '300px';
+textarea.focus();
+body.append(textarea);
+
+// Информация
+const info = document.createElement('div');
+info.classList.add('info');
+body.append(info);
+info.innerText = 'Для преключения раскладки нажмите клавишу \'⌘\'';
+
+textarea.addEventListener('click', () => {
+    panel.classList.add('top');
+    openKeyboard.innerText = 'Закрыть клавиатуру';
+    isBtn = true;
+    openKeyboard.style.backgroundColor = 'red';
+});
+
+openKeyboard.addEventListener('click', () => {
+    panel.classList.toggle('top');
+    if (!isBtn) {
+        openKeyboard.innerText = 'Закрыть клавиатуру';
+        isBtn = true;
+        openKeyboard.style.backgroundColor = 'red';
+    } else {
+        openKeyboard.innerText = 'Открыть клавиатуру';
+        isBtn = false;
+        openKeyboard.style.backgroundColor = 'blue';
+    }
+});
+
+// Блок клавишь
+const keyboard = document.createElement('div');
+keyboard.classList.add('keyboard');
+panel.append(keyboard);
+
+// Индикатор Caps
+const quad = document.createElement('div');
+
+// Объявим переменную содержащую тект
+let text = '';
+// Флаг переключения языков
+let lang = JSON.parse(localStorage.getItem('key')) || false;
+// Состояние Caps
+let caps = false;
+
+// Локальное хранилище
+const storage = () => {
+    localStorage.setItem('key', lang);
+};
+
+// Методы клавиатуры
+const methods = {
+    backspace(a) {
+        text = a.split('').splice(0, text.length - 1).join('');
+    },
+    tab(a) {
+        text = ' ' + a;
+    },
+    metaleft(a) {
+        if (!a) {
+            createBtn(en);
+        } else {
+            createBtn(ru);
+        }
+        storage();
+    },
+    enter(a) {
+    },
+    capslock(a) {
+        if (caps) {
+            quad.classList.add('quad');
+            a.append(quad);
+        } else {
+            quad.remove();
+        }
+    },
+    space() {
+        text += ' ';
+    },
+};
+
+function createBtn(data) {
+    for (let i = 0; i < data.length; i += 1) {
+        const btn = document.createElement('button');
+        btn.classList.add('btn');
+        btn.setAttribute('data', data[i].code);
+        keyboard.append(btn);
+
+        btn.style.width = data[i].width;
+        btn.innerText = String(data[i].key);
+        btn.style.backgroundColor = data[i].color;
+
+        btn.addEventListener('click', () => {
+            const atr = btn.getAttribute('data');
+            if (atr === 'Backspace') {
+                methods[`${atr}`.toLowerCase()](text);
+                textarea.innerText = text;
+            } else if (atr === 'Tab') {
+                methods[`${atr}`.toLowerCase()](text);
+                textarea.innerText = text;
+            } else if (atr === 'MetaLeft') {
+                keyboard.innerHTML = '';
+                lang = !lang;
+                methods[`${atr}`.toLowerCase()](lang);
+            } else if (atr === 'CapsLock') {
+                caps = !caps;
+                methods[`${atr}`.toLowerCase()](btn);
+            } else if (atr === 'Enter') {
+                methods[`${atr}`.toLowerCase()](text);
+                textarea.innerText = text;
+            } else if (atr === 'Space') {
+                methods[`${atr}`.toLowerCase()](text);
+                textarea.innerText = text;
+            } else if (atr === 'ControlLeft' || atr === 'ControlRight' || atr === 'AltLeft' || atr === 'ShiftLeft'
+                || atr === 'ShiftRight' || atr === 'AltRight' || atr === 'NumpadDecimal') {
+                text += '';
+                textarea.innerText = text;
+            } else if (caps === false) {
+                text += data[i].key.toLowerCase();
+                textarea.innerText = text;
+            } else if (caps === true) {
+                btn.style.backgroundColor = 'red';
+                text += data[i].key.toUpperCase();
+                textarea.innerText = text;
+            }
+
+            document.querySelectorAll('.btn').forEach((e) => {
+                e.style.backgroundColor = '';
+            });
+            btn.style.backgroundColor = 'blue';
+        });
+    }
+}
+methods.metaleft(lang);
+
+window.addEventListener('keydown', (event) => {
+    if (event.key !== 'Backspace' && event.key !== 'Tab' && event.key !== 'CapsLock' && event.key !== 'Shift'
+        && event.key !== 'Control' && event.key !== 'Alt' && event.key !== 'Meta') {
+        text += event.key.toLowerCase();
+        textarea.innerText = text;
+    }
+    if (!document.querySelector('.btn[data ="'+ event.code +'"]')) {
+        return
+    }
+
+    document.querySelectorAll('.btn').forEach((el) => {
+        el.style.backgroundColor = '';
+    });
+
+    document.querySelector('.btn[data ="'+event.code+'"]').style.backgroundColor = 'blue';
+});
